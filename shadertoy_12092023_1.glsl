355c4cc4c018c2d4e471c9bd3ec01d9eb88d706d
ledoriang <ledorian.g@gmail.com>
2023-09-12 19:57:56 +0100

    daily shader

---
+++ shadertoy_12092023_1.glsl
@@ -0,0 +1,27 @@
+// Following youtube tutorial: https://www.youtube.com/watch?v=f4s1h2YETNY
+
+//https://iquilezles.org/articles/palettes/
+vec3 palette( float t ) {
+    vec3 a = vec3(0.5, 0.5, 0.5);
+    vec3 b = vec3(0.5, 0.5, 0.5);
+    vec3 c = vec3(1.0, 1.0, 1.0);
+    vec3 d = vec3(0.263,0.416,0.557);
+
+    return a + b*cos( 6.28318*(c*t+d) );
+}
+void mainImage( out vec4 fragColor, in vec2 fragCoord )
+{
+   vec2 uv = (fragCoord * 2.0 - iResolution.xy) / iResolution.y;
+   
+   float d = length(uv);
+   vec3 col = palette(d);
+   
+   d = sin(d*8. + iTime*1.5)/8.;
+   d = abs(d);
+
+   d = 0.02 / d;
+
+   col *= d;
+   
+   fragColor = vec4( col, 1.0);
+}  
